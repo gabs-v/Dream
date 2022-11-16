9069323b6b237f5262d971c74f0a4161609ae7bb
gabs-v <gabyvazquez8297@gmail.com>
2022-11-15 21:18:19 -0800

    home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+const Home = (props) => {
+    const {theDreams, setTheDreams} = props
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/dreams")
+        .then((res) => {
+            console.log(res.data)
+            setTheDreams(res.data)
+        })
+        .catch((err) => console.log(err))
+    },[])
+
+    return (
+        <div>
+            <div className='navBar'>
+                <h1>Dazed and Dreamin'</h1>
+                <ul>
+                    <li> All Dreams </li>
+                    <li> New Entry </li>
+                    <li> Favorite Dreams </li>
+                </ul>
+            </div>
+            <div className='allEntries'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
