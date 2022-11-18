1e7b4fe46d988eb0ee5a44f90dd99cd2b42dba4b
gabs-v <gabyvazquez8297@gmail.com>
2022-11-17 22:57:10 -0800

    Form

--- client/src/components/New.js
+++ client/src/components/New.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 
-const New = () => {
+const New = (props) => {
+    const [subjectLine, setSubjectLine] = useState('');
+    const [description,setDescription] = useState('');
+
+    useEffect(() => {
+        axios.post("http://localhost:8000/api/dreams")
+        .then((res) => {
+            console.log(res)
+            res.json(res)
+        })
+        .catch((err) => console.log(err))
+        })
+
+
     return (
-        <div>New</div>
+        <div>
+            <div className='navBar'>
+                <h1>Dazed and Dreamin'</h1>
+                <ul>
+                    <li> All Dreams </li>
+                    <li> New Entry </li>
+                    <li> Favorite Dreams </li>
+                </ul>
+            </div>
+            <form>
+                <div className='formSubject'>
+                    <label>Subject Line:</label>
+                    <input type='text' />
+                </div>
+                <div className='formSubject'>
+                    <label>Description:</label>
+                    <textarea cols='20' rows='5' />
+                </div>
+            </form>
+        </div>
     )
 }
 
